38c2142fedb29fb5980b0dada6514f024a80b8ef
maelpasq <mael.pasqueraud@gmail.com>
2022-07-20 15:40:25 +0200

    fix mobile

--- js/app.js
+++ js/app.js
@@ -9,3 +9,22 @@ window.onscroll = function () {
 function CloseModal() {
   document.getElementById('modal').style.display = 'none';
 }
+
+function isMobileDevice() { 
+  if( navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+  ){
+    function DeviceMobile() {
+      document.getElementById('page').style.display = 'none';
+    }
+  } else {
+    function DeviceMobile() {
+      document.getElementById('page').style.display = 'flex';
+    }
+  }
+}
